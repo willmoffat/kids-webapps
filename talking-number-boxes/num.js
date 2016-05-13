5e3460c43d433d61e1240401d4383c4b03141afd
Will Moffat <will@hamstersoup.com>
2016-05-13 09:52:14 +0200

    Live Reload support.

--- talking-number-boxes/num.js
+++ talking-number-boxes/num.js
@@ -80,3 +80,23 @@ $num.addEventListener('input', show);
 $num.addEventListener('blur', function() { $num.focus(); });
 $num.value = '10';
 resize();
+
+
+
+//////////////////////////////////////////////////////////////////////////////
+(function liveReload() {
+  if (!document.location.search.includes('reload')) {
+    return;
+  }
+  var s = document.querySelectorAll('script'), src = s[s.length - 1].src, lm;
+  var compare = function(r) {
+    if (lm && lm !== r.headers.get('Last-Modified')) {
+      return document.location.reload();
+    }
+    lm = r.headers.get('Last-Modified');
+    return setTimeout(get, 1000);
+  };
+  var get = function() { fetch(src, {method: 'HEAD'}).then(compare); };
+  get();
+})();
+//////////////////////////////////////////////////////////////////////////////
