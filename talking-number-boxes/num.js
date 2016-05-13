94a1872ed7d92ce854527087af228012a707c5b5
Will Moffat <will@hamstersoup.com>
2016-05-13 07:00:39 +0200

    Use canvas for drawing.

--- talking-number-boxes/num.js
+++ talking-number-boxes/num.js
@@ -1,31 +1,58 @@
 var $num = document.getElementById('num');
 var $formatted = document.getElementById('formatted');
-var $container = document.getElementById('container');
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
 
 function parse(val) {
   val = val.replace(/,/g, '');
   return parseInt(val, 10);
 }
 
+function resize() {
+  // Set the internal size to match CSS size.
+  canvas.width = canvas.offsetWidth;
+  canvas.height = canvas.offsetHeight; // HACK?
+  show();
+}
+
+var W = 5;
+var H = 5;
+var SX = W + 1;
+var SY = H + 1;
+var MAXY;
+
 function boxes(n) {
-  if (n > 10000) {
-    return 'Too big to show!';
+  var MAXX = canvas.width;
+  ctx.clearRect(0, 0, MAXX, MAXY);
+  var perLine = Math.ceil(MAXX / SX);
+  var MAXY = Math.ceil(n / perLine) * SY;
+  if (MAXY > 32000) {
+    return;
   }
-  if (n < 1) {
-    return '';
+  canvas.style.height = MAXY + 'px';
+  canvas.height = MAXY;
+  var x = 0, y = 0;
+  for (var i = 0; i < n; i++) {
+    ctx.fillStyle = (i % 10 === 0) ? "red" : "pink";
+    ctx.fillRect(x, y, W, H);
+    x += SX;
+    if (x > MAXX) {
+      x = 0;
+      y += SY;
+      if (y >= MAXY) {
+        break;  // HACK ?
+      }
+    }
   }
-  var a = new Array(n + 1);
-  return a.join('<span></span>');
 }
 
 function showNaN() {
   $formatted.textContent = 'Not a number';
-  $container.innerHTML = '';
 }
 
 function showNum(n, nStr) {
   $formatted.textContent = nStr;
-  $container.innerHTML = boxes(n);
+  boxes(n);
   speak(nStr);
 }
 
@@ -48,7 +75,9 @@ function speak(txt) {
   speechSynthesis.speak(msg);
 }
 
+
+window.addEventListener('resize', resize);
 $num.addEventListener('input', show);
 $num.addEventListener('blur', function() { $num.focus(); });
 $num.value = '10';
-show();
+resize();
