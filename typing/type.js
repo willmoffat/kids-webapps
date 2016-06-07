e411bd25e9735e305ad31003132513f23fe3175a
Will Moffat <will@hamstersoup.com>
2016-06-07 13:18:51 +0200

    whitespace

--- typing/type.js
+++ typing/type.js
@@ -1,75 +1,76 @@
 (function() {
-    "use strict";
-var DEBUG = true;
-var wordGuide = document.getElementById('wordGuide');
-var wordInput = document.getElementById('wordInput');
+  "use strict";
+  var DEBUG = true;
+  var wordGuide = document.getElementById('wordGuide');
+  var wordInput = document.getElementById('wordInput');
 
-function init() {
-  initEventListeners();
-  // fullScreen(); TODO(wdm)
-  changeGuideWord('My name is Yann');
-}
-
-window.addEventListener('load', init);
-
-function speak(txt) {
-  speechSynthesis.cancel();
-  var msg = new SpeechSynthesisUtterance(txt);
-  msg.lang = 'en-GB'; // For some reason, my default voice is German.
-  speechSynthesis.speak(msg);
-}
-
-function speakLetter(e) {
-  var letter = e.data;
-  speak(letter.toUpperCase());
-}
-
-function changeGuideWord(toType) {
-  speak(toType);
-  wordGuide.textContent = toType;
-  wordInput.value = '';
-}
-
-function onKey() {
-  var got = wordInput.value.toUpperCase();
-  var want = wordGuide.textContent.toUpperCase();
-  if (got === want) {
-    console.log('TODO: success!');
-    speak(want);
+  function init() {
+    initEventListeners();
+    // fullScreen(); TODO(wdm)
+    changeGuideWord('My name is Yann');
   }
-  var typo = want.slice(0, got.length) !== got;
-  wordInput.className = typo ? 'typo' : '';
-}
 
-function initEventListeners() {
-  wordInput.addEventListener('textInput', speakLetter, false);
-  wordInput.addEventListener('keyup', onKey, false);
-  wordInput.addEventListener('keydown', trapModifierKeys, false);
+  window.addEventListener('load', init);
 
-  if (!DEBUG) {
-    return;
+  function speak(txt) {
+    speechSynthesis.cancel();
+    var msg = new SpeechSynthesisUtterance(txt);
+    msg.lang = 'en-GB'; // For some reason, my default voice is German.
+    speechSynthesis.speak(msg);
   }
-  // Disable mouse.
-  var elements = Array.prototype.slice.call(document.querySelectorAll('*'));
-  ['contextmenu', 'mousedown', 'mouseup', 'click'].forEach(function(eventName) {
-    elements.forEach(function(el) {
-      el.addEventListener(eventName, trapEvent, true);
+
+  function speakLetter(e) {
+    var letter = e.data;
+    speak(letter.toUpperCase());
+  }
+
+  function changeGuideWord(toType) {
+    speak(toType);
+    wordGuide.textContent = toType;
+    wordInput.value = '';
+  }
+
+  function onKey() {
+    var got = wordInput.value.toUpperCase();
+    var want = wordGuide.textContent.toUpperCase();
+    if (got === want) {
+      console.log('TODO: success!');
+      speak(want);
+    }
+    var typo = want.slice(0, got.length) !== got;
+    wordInput.className = typo ? 'typo' : '';
+  }
+
+  function initEventListeners() {
+    wordInput.addEventListener('textInput', speakLetter, false);
+    wordInput.addEventListener('keyup', onKey, false);
+    wordInput.addEventListener('keydown', trapModifierKeys, false);
+
+    if (!DEBUG) {
+      return;
+    }
+    // Disable mouse.
+    var elements = Array.prototype.slice.call(document.querySelectorAll('*'));
+    ['contextmenu', 'mousedown', 'mouseup', 'click'].forEach(function(
+        eventName) {
+      elements.forEach(function(el) {
+        el.addEventListener(eventName, trapEvent, true);
+      });
     });
-  });
-}
-
-function trapModifierKeys(e) {
-  if (DEBUG) {
-    return;
   }
-  if (e.metaKey || e.ctrlKey || e.altGraphKey || e.altKey || e.which === 9) {
-    console.log('trapping ', e);
-    trapEvent(e);
-  }
-}
 
-function trapEvent(e) {
-  e.preventDefault();
-  e.stopPropagation();
-}
+  function trapModifierKeys(e) {
+    if (DEBUG) {
+      return;
+    }
+    if (e.metaKey || e.ctrlKey || e.altGraphKey || e.altKey || e.which === 9) {
+      console.log('trapping ', e);
+      trapEvent(e);
+    }
+  }
+
+  function trapEvent(e) {
+    e.preventDefault();
+    e.stopPropagation();
+  }
 })();
